538893b69f6f7a25ac82b7acf9eca74fab51a08d
Allen Vu <allenvu4838@gmail.com>
2023-11-10 16:50:27 -0800

    update

--- grade.sh
+++ grade.sh
@@ -14,3 +14,10 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+set -e
+cp ./student-submission/ListExamples.java ./GradeServer.java ./Server.java ./TestListExamples.java ./grading-area/    
+
+cd ./grading-area
+javac -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar *.java
+java -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
